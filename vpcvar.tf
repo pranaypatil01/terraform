4937102781cb19b5c0fa6cb7202de818af708a98
Pranay Prakash Patil <153298938+pranaypatil01@users.noreply.github.com>
2024-05-03 15:45:48 +0530

    Update vpcvar.tf

--- vpcvar.tf
+++ vpcvar.tf
@@ -10,9 +10,13 @@ variable "My_IGW"{
     type=any
     default="aws_Internet_Gateway.MyIGW"
 }
-variable "vpc_subnet"{
+variable "vpc_subnet_1"{
 type=any
 default="10.0.0.0/17"
+}
+variable "vpc_subnet_2"{
+type=any
+default="10.0.0.0/18"
 }
 variable "subnet_tag"{
     type=any
